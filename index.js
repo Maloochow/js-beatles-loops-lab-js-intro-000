cbb301b15f55c0578853ce6bc4bce88220081f06
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-14 01:56:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Beatles = []
 
 function theBeatlesPlay (musician, instrument) {
   for (i = 0, i < musician.length, i += 1) {
-    
+    Beatles.push(`${musician(i)} plays ${instrument(i)}`)
   }
+  return Beatles[];
 }
